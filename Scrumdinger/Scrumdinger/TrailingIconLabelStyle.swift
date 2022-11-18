8183ef7106a9b0e1cea66f2f0949e3ee05d4cbd2
Drahoš Maďar <drahoslav.madar@gmail.com>
2022-11-18 15:01:35 +0100

    Meetings View, Scrums View, Detail View, Card View, routing

---
+++ Scrumdinger/Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by DMA on 18/11/2022.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
